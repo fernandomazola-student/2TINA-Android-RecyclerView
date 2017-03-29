4271940b5fd3996369ea6b1f5b510510c5f060f2
Fernando Mazola <fernando_mazola@hotmail.com>
2017-03-29 11:08:55 -0300

    Finalizado o contatoadapter e implmentado todos os metodos

--- app/src/main/java/br/com/fiap/a2tina_android_recyclerview/ContatoAdapater.java
+++ app/src/main/java/br/com/fiap/a2tina_android_recyclerview/ContatoAdapater.java
@@ -3,6 +3,7 @@ package br.com.fiap.a2tina_android_recyclerview;
 import android.content.Context;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -26,18 +27,26 @@ public class ContatoAdapater extends RecyclerView.Adapter<ContatoAdapater.Contat
 
     @Override
     public ContatoViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
         //Reposavel por criar e instanciar o viewholder
+        View v = LayoutInflater.from(this.context).inflate(R.layout.recyclerview_contato, parent, false);
+        ContatoViewHolder contatoViewHolder = new ContatoViewHolder(v);
+
+        return null;
+
     }
 
     @Override
     public void onBindViewHolder(ContatoViewHolder holder, int position) {
-    //Para cada item que tem na lista, e coloco no viewholder
+    //Para cada item que tem na lista, e coloco no viewholder // Pegar os dados do contatos que vamos receber
+        Contato contato = this.contatos.get(position);
+        holder.imgContato.setImageResource(contato.getImagem());
+        holder.txtNome.setText(contato.getNome());
+        holder.txtStatus.setText(contato.getStatus());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return this.contatos.size();
         //Quantidade de itens que vao ser renderizados na lista
     }
 
@@ -50,6 +59,11 @@ public class ContatoAdapater extends RecyclerView.Adapter<ContatoAdapater.Contat
 
         public ContatoViewHolder(View itemView) {
             super(itemView);
+
+            cardView = (CardView) itemView;
+            imgContato = (ImageView) itemView.findViewById(R.id.imgContato);
+            txtNome =  (TextView) itemView.findViewById(R.id.txtNome);
+            txtStatus = (TextView) itemView.findViewById(R.id.txtStatus);
         }
     }
 }
