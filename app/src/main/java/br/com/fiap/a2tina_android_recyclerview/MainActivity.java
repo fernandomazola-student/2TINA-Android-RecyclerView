4f2015ba9ccbb02bae6c4f244daa9124f330de81
Fernando Mazola <fernando_mazola@hotmail.com>
2017-03-29 10:35:51 -0300

    Adicionando tamanho fixo na recyclerview

--- app/src/main/java/br/com/fiap/a2tina_android_recyclerview/MainActivity.java
+++ app/src/main/java/br/com/fiap/a2tina_android_recyclerview/MainActivity.java
@@ -2,12 +2,20 @@ package br.com.fiap.a2tina_android_recyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView rvContatos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rvContatos = (RecyclerView) findViewById(R.id.rvContatos);
+        rvContatos.setHasFixedSize(true);
     }
+
+
 }
