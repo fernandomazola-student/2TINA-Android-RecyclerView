fa7a9f3e43dde09e8afe7c50aa0fe819d9eb1fdc
Fernando Mazola <fernando_mazola@hotmail.com>
2017-03-29 11:20:32 -0300

    Adicionado na mainactivity todos os itens necessarios para montar a recycler view

--- app/src/main/java/br/com/fiap/a2tina_android_recyclerview/MainActivity.java
+++ app/src/main/java/br/com/fiap/a2tina_android_recyclerview/MainActivity.java
@@ -2,20 +2,32 @@ package br.com.fiap.a2tina_android_recyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView rvContatos;
-
+    List<Contato> contatos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        contatos = GeraContato.listaContatos();
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+
+        ContatoAdapater adapater = new ContatoAdapater(this, contatos);
+
+
         rvContatos = (RecyclerView) findViewById(R.id.rvContatos);
         rvContatos.setHasFixedSize(true);
+        rvContatos.setAdapter(adapater);
+        rvContatos.setLayoutManager(layoutManager);
     }
 
 
